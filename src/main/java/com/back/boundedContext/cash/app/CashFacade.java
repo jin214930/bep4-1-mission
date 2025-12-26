00d94c15d42697aab5802e80fabcec4c119b1bb6
jin214930 <jin214930@naver.com>
2025-12-26 09:25:17 +0900

    0023

---
+++ src/main/java/com/back/boundedContext/cash/app/CashFacade.java
@@ -0,0 +1,29 @@
+package com.back.boundedContext.cash.app;
+
+import com.back.boundedContext.cash.domain.CashMember;
+import com.back.boundedContext.cash.out.CashMemberRepository;
+import com.back.shared.member.dto.MemberDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CashFacade {
+    private final CashMemberRepository cashMemberRepository;
+
+    @Transactional
+    public CashMember syncMember(MemberDto memberDto) {
+        CashMember member = new CashMember(
+                memberDto.getId(),
+                memberDto.getCreatedDate(),
+                memberDto.getModifiedDate(),
+                memberDto.getUsername(),
+                "",
+                memberDto.getNickname(),
+                memberDto.getActiveScore()
+        );
+
+        return cashMemberRepository.save(member);
+    }
+}
---
+++ src/main/java/com/back/boundedContext/cash/in/CashEventListener.java
@@ -0,0 +1,30 @@
+package com.back.boundedContext.cash.in;
+
+import com.back.boundedContext.cash.app.CashFacade;
+import com.back.shared.post.event.MemberJoinedEvent;
+import com.back.shared.post.event.MemberModifiedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class CashEventListener {
+    private final CashFacade cashFacade;
+
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void handle(MemberJoinedEvent event) {
+        cashFacade.syncMember(event.getMemberDto());
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void handle(MemberModifiedEvent event) {
+        cashFacade.syncMember(event.getMemberDto());
+    }
+}
